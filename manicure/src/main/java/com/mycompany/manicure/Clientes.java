74d23c5250c3ebd2a471a64eaf245c8a13b30223
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-09-04 11:40:40 -0300

    feat: new java aplication

---
+++ manicure/src/main/java/com/mycompany/manicure/Clientes.java
@@ -0,0 +1,7 @@
+
+package com.mycompany.manicure;
+
+
+public class Clientes {
+    
+}
---
+++ manicure/src/main/java/com/mycompany/manicure/Manicure.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.manicure;
+
+/**
+ *
+ * @author Alexsander
+ */
+public class Manicure {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
