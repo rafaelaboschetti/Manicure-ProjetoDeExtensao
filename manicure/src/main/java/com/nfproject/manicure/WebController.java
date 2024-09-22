f7f4d002d64f2b8450da7b4a9e462cb4f1ca458c
Alexsander Marchi Zunino <alexsandermarchi@gmail.com>
2024-09-21 21:31:13 -0300

    passing info to json and reloading classes

--- manicure/src/main/java/com/nfproject/manicure/WebController.java
+++ manicure/src/main/java/com/nfproject/manicure/WebController.java
@@ -1,14 +1,27 @@
 package com.nfproject.manicure;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class WebController {
     
-    @RequestMapping("/hello")
-    public String sayHello(){
-        System.out.println("Saying hello... olá");
-        return "hello big fallout player";
+    
+    @GetMapping("/agendamentos")
+    public List<HoraMarcada> getAgendamentos() {
+        Cliente anaBeatriz = new Cliente("Ana", "Beatriz");
+        Cliente carlaFontes = new Cliente("Carla", "Fontes");
+        
+        HoraMarcada horaAna = new HoraMarcada("11 de Outubro", "11:00", anaBeatriz);
+        HoraMarcada horaCarla = new HoraMarcada("10 de Outubro", "12:00", carlaFontes);
+        
+        List<HoraMarcada> agendamentoGerais = new ArrayList<>();
+        agendamentoGerais.add(horaAna);
+        agendamentoGerais.add(horaCarla);
+        
+        return agendamentoGerais;
     }
 }
